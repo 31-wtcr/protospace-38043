52c8863c5d7505e539994d6c7643009066669b74
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-06-06 22:06:10 +0900

    comments_controller 空欄で投稿したらもとの表示に戻るよう修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(params[:prototype_id])
     else
-      @prototype = Prototype.find(params[:prototype_id])
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
